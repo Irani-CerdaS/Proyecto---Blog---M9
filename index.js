aa836a0a10723b2c9a50917d6da7cf4c8402fb96
Irani Cerda <Iranialiceaustin@gmail.com>
2026-04-01 10:37:41 -0300

    Commit 6: Definición de asociaciones y relaciones entre modelos (User-News-Comment)

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const app = require('./src/server');
 const sequelize = require('./src/database');
 
+const User = require('./src/models/User');
+const News = require('./src/models/News');
+const Comment = require('./src/models/Comment');
+
+// Relaciones
+User.hasMany(News, { foreignKey: 'userId' });
+News.belongsTo(User, { foreignKey: 'userId' });
+
+News.hasMany(Comment, { foreignKey: 'newsId' });
+Comment.belongsTo(News, { foreignKey: 'newsId' });
+
+User.hasMany(Comment, { foreignKey: 'userId' });
+Comment.belongsTo(User, { foreignKey: 'userId' });
+
 const start = async () => {
   try {
-    // Autenticar la conexión a la base de datos
     await sequelize.authenticate();
     console.log('Autenticación con la base de datos exitosa.');
 
-    // Sincronizar los modelos
+    // Sincronizar los modelos con sequelize.sync()
     await sequelize.sync({ force: false });
-    console.log('Modelos de Sequelize sincronizados correctamente.');
+    console.log('Modelos de Sequelize sincronizados correctamente con las relaciones definidas.');
 
-    // Poner el servidor a escuchar
     app.listen(3000, () => {
       console.log('Servidor escuchando en el puerto 3000');
     });
